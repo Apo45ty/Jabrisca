7ca43646354766f9c825a4fb99725d6d071aba55
Antonio Ahmed Tapia <antonio.tapia@upr.edu>
2025-02-05 22:21:15 -0400

    Added code for unit test card comparator

--- src/edu/uprm/ece/icom4015/jabrisca/test/unit/BriscaCardComparatorTest.java
+++ src/edu/uprm/ece/icom4015/jabrisca/test/unit/BriscaCardComparatorTest.java
@@ -1,4 +1,74 @@
 package edu.uprm.ece.icom4015.jabrisca.test.unit;
 
+import edu.uprm.ece.icom4015.jabrisca.server.game.brica.BriscaCardComparator;
+import edu.uprm.ece.icom4015.jabrisca.server.game.brica.ItalianDeckCard;
+import org.junit.jupiter.api.Test;
+
 public class BriscaCardComparatorTest {
+
+    @Test
+    public void test_compare_HappyPathCardOneIsSuit(){
+        //Prepare
+        ItalianDeckCard cardOne = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard cardTwo = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.bastion,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard.ItalianDeckSuit suit = ItalianDeckCard.ItalianDeckSuit.cup;
+
+        //Execute
+        int result = BriscaCardComparator.compare(cardOne,cardTwo,suit);
+        assert (result==1);
+    }
+    @Test
+    public void test_compare_HappyPathCardTwoIsSuit(){
+        //Prepare
+        ItalianDeckCard cardOne = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard cardTwo = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.bastion,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard.ItalianDeckSuit suit = ItalianDeckCard.ItalianDeckSuit.bastion;
+
+        //Execute
+        int result = BriscaCardComparator.compare(cardOne,cardTwo,suit);
+        assert (result==-1);
+    }
+    @Test
+    public void test_compare_HappyPathNoCardIsSuitRankIsMatch(){
+        //Prepare
+        ItalianDeckCard cardOne = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard cardTwo = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard.ItalianDeckSuit suit = ItalianDeckCard.ItalianDeckSuit.bastion;
+
+        //Execute
+        int result = BriscaCardComparator.compare(cardOne,cardTwo,suit);
+        assert (result==0);
+    }
+    @Test
+    public void test_compare_HappyPathNoCardIsSuitRankIsNotMatch(){
+        //Prepare
+        ItalianDeckCard cardOne = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard cardTwo = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.tre);
+        ItalianDeckCard.ItalianDeckSuit suit = ItalianDeckCard.ItalianDeckSuit.bastion;
+
+        //Execute
+        int result = BriscaCardComparator.compare(cardOne,cardTwo,suit);
+        assert (result==-8);
+    }
+    @Test
+    public void test_compare_HappyPathBothCardsIsSuitRankIsMatch(){
+        //Prepare
+        ItalianDeckCard cardOne = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard cardTwo = new ItalianDeckCard(ItalianDeckCard.ItalianDeckSuit.cup,
+                ItalianDeckCard.ItalianDeckRank.five);
+        ItalianDeckCard.ItalianDeckSuit suit = ItalianDeckCard.ItalianDeckSuit.cup;
+
+        //Execute
+        int result = BriscaCardComparator.compare(cardOne,cardTwo,suit);
+        assert (result==0);
+    }
 }
